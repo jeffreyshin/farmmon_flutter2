6792c3aacb2b6c322fa7ce8069551fd52bf5041b
JaeHoon Shin <shinj@korea.kr>
2023-05-09 22:32:40 +0900

    menu type

---
+++ lib/_main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Welcome to Flutter, title',
+        home: Scaffold(
+          appBar: AppBar
+            (
+            elevation: 2.0,
+            backgroundColor: Colors.white,
+            title: Text('딸기 탄저병 예측', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 30.0)),
+            actions: <Widget>
+            [
+              Container
+                (
+                margin: EdgeInsets.only(right: 8.0),
+                child: Row
+                  (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>
+                  [
+                    Text('FarmBiz.xyz', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w700, fontSize: 14.0)),
+                    Icon(Icons.arrow_drop_down, color: Colors.black54)
+                  ],
+                ),
+              )
+            ],
+          ),
+          body: MyBody(),
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {},
+                ),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),      )
+    );
+  }
+}
+
+
+
+class MyBody extends StatelessWidget {
+  const MyBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(''),
+            const Text('딸기 탄저병 예측 결과',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 25,
+                )
+            ),
+            const Text('1. iot포털 환경데이터 가져오기', style: TextStyle(fontSize: 15),),
+            const Text('2. 환경데이터 파일 작성', style: TextStyle(fontSize: 15),),
+            const Text('3. 탄저병 예측 API호출', style: TextStyle(fontSize: 15),),
+            const Text('4. 예측결과를 출력합니다.', style: TextStyle(fontSize: 15),),
+          ],
+        )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,224 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to Flutter, title',
-        home: Scaffold(
-          appBar: AppBar
-            (
-            elevation: 2.0,
-            backgroundColor: Colors.white,
-            title: Text('딸기 탄저병 예측', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 30.0)),
-            actions: <Widget>
-            [
-              Container
-                (
-                margin: EdgeInsets.only(right: 8.0),
-                child: Row
-                  (
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>
-                  [
-                    Text('FarmBiz.xyz', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w700, fontSize: 14.0)),
-                    Icon(Icons.arrow_drop_down, color: Colors.black54)
-                  ],
-                ),
-              )
-            ],
-          ),
-          body: MyBody(),
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          ),      )
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'FarmMon App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: MyHomePage(),
+      ),
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 
-class MyBody extends StatelessWidget {
-  const MyBody({Key? key}) : super(key: key);
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(left: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const Text(''),
-            const Text('딸기 탄저병 예측 결과',
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 25,
-                )
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      case 2:
+        page = GeneratorPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.apple),
+                    label: Text('딸기'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('토마토'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('파프리카'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
             ),
-            const Text('1. iot포털 환경데이터 가져오기', style: TextStyle(fontSize: 15),),
-            const Text('2. 환경데이터 파일 작성', style: TextStyle(fontSize: 15),),
-            const Text('3. 탄저병 예측 API호출', style: TextStyle(fontSize: 15),),
-            const Text('4. 예측결과를 출력합니다.', style: TextStyle(fontSize: 15),),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
           ],
-        )
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    var now = new DateTime.now(); 
+    String formatDate = DateFormat('yy년 MM월 dd일').format(now);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('딸기 탄저병', style: TextStyle(fontSize: 25),),
+          SizedBox(height: 10),
+          Text('$formatDate'),
+          SizedBox(height: 10),
+          Text('1. iot포털 환경데이터 가져오기', style: TextStyle(fontSize: 15),),
+          Text('2. 환경데이터 파일 작성', style: TextStyle(fontSize: 15),),
+          Text('3. 탄저병 예측 API호출', style: TextStyle(fontSize: 15),),
+          Text('4. 예측결과를 출력합니다.', style: TextStyle(fontSize: 15),),
+          SizedBox(height: 20),
+          BigCard(pair: pair),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                child: Text('전날'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('다음날'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          "예측결과",
+          style: style,
+          semanticsLabel: "탄저병 예측 결과 차트",
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
